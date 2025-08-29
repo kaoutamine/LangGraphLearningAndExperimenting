806fa77a161e54bf0d941dbac3d8ab6d5ac48099
kaoutamine <kaourintin.tamine@jobcloud.ch>
2025-08-29 21:24:57 +0200

    add tooling (homemade) to the LLM on langchain tutorial

--- chatbot_tutorial/ChatBot.py
+++ chatbot_tutorial/ChatBot.py
@@ -6,6 +6,8 @@ from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 from IPython.display import Image, display
 
+from langchain_tavily import TavilySearch
+from BasicToolNode import BasicToolNode
 import os
 from langchain.chat_models import init_chat_model
 
@@ -17,18 +19,54 @@ class State(TypedDict):
 
 
 load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
+
+def chatbot(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+def route_tools(state: State):
+    """
+    Route the tools based on the state (either END or tools)
+    """
+    if isinstance(state, list):
+        ai_message = state[-1]
+    elif messages := state.get("messages", []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError(f"No messages found in input state to tool_edge: {state}")
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        return "tools"
+    return END
+
 
 
 llm = init_chat_model("openai:gpt-4.1")
 
-def chatbot(state: State):
-    return {"messages": [llm.invoke(state["messages"])]}
+tool = TavilySearch(max_results=2)
+tools = [tool]
+
+llm_with_tools = llm.bind_tools(tools)
 
 graph_builder = StateGraph(State)
-graph_builder.add_node("chatbot", chatbot)  # You need to add the node first!
+
+#add chatbot node and end/start
+graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_edge(START, "chatbot")
 graph_builder.add_edge("chatbot", END)
+
+
+tool_node = BasicToolNode(tools=[tool])
+
+graph_builder.add_node("tools", tool_node)
+graph_builder.add_conditional_edges(
+    "chatbot",
+    route_tools,
+    # The following dictionary lets you tell the graph to interpret the condition's outputs as a specific node
+    # It defaults to the identity function, but if you
+    # want to use a node named something else apart from "tools",
+    # You can update the value of the dictionary to something else
+    # e.g., "tools": "my_tools"
+    {"tools": "tools", END: END},
+)
 graph = graph_builder.compile()
 
 try:
